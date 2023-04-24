b556f120815f8b5718e062e54ea40f97d88f2800
Luís Henrique <luishsilva09@gmail.com>
2023-04-24 20:33:59 -0300

    fix: use routes on app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import routes from "./routes";
 
 const app = express();
 app.use(cors(), express.json());
+app.use(routes);
 
 export default app;
